3090621d04108a9a485676d3d63e531a3383a6e3
Armen <armenasryan@gmail.com>
2023-01-10 17:23:58 +0300

    java

--- src/HomeWork5/ArraySpaceExample.java
+++ src/HomeWork5/ArraySpaceExample.java
@@ -2,8 +2,11 @@ package HomeWork5;
 
 public class ArraySpaceExample {
     public static void main(String[] args) {
-        char[] spaceArray = { ' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
+        char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
+        char[] result = spaceArray;
+        int d = 0;
         int c = 0;
+        boolean y = false;
         boolean x = false;
         for (int i = 0; i < spaceArray.length; i++) {
             while (spaceArray[i] != ' ') {
@@ -16,11 +19,25 @@ public class ArraySpaceExample {
         if (x != false) {
             c++;
         }
-        for (int n = c ; n < spaceArray.length; n++) {
-            System.out.print(spaceArray[n]);
+        for (int i = spaceArray.length - 1; i > c; i--) {
+            while (spaceArray[i] != ' ') {
+                y = true;
+                if (spaceArray[i] != ' ') {
+                    break;
+                }
+            }
         }
+        if (x != false) {
+            d++;
+        }
+        for (int i = c; i < result.length - d; i++) {
+            System.out.print(result[i]);
+        }
+
     }
+
 }
+
 
 
 
